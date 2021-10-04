d5dd0a79e18a55a0f98486cb937fee69eebcbcfc
tehstone <stone.johnny@gmail.com>
2021-10-04 16:12:32 -0700

    incrementing version

--- wayfarer-keyboard-review.user.js
+++ wayfarer-keyboard-review.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         Wayfarer Keyboard Review
-// @version      0.3.0
+// @version      0.3.1
 // @description  Add keyboard review to Wayfarer
 // @namespace    https://github.com/tehstone/wayfarer-addons
 // @downloadURL  https://github.com/tehstone/wayfarer-addons/raw/main/wayfarer-keyboard-review.user.js
