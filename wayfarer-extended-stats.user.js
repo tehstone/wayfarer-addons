2e08bd278117fb175014689a90b24802c8503946
tehstone <stone.johnny@gmail.com>
2021-10-19 16:17:27 -0700

    version bump

--- wayfarer-extended-stats.user.js
+++ wayfarer-extended-stats.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         Wayfarer Extended Stats
-// @version      0.3.3
+// @version      0.3.4
 // @description  Add extended Wayfarer Profile stats
 // @namespace    https://github.com/tehstone/wayfarer-addons/
 // @downloadURL  https://github.com/tehstone/wayfarer-addons/raw/main/wayfarer-extended-stats.user.js
