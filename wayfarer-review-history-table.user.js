77210dfe381f9e180127274e8af975bc60fe6dd8
dustinyeeaah <58644408+dustin-heider@users.noreply.github.com>
2023-02-14 20:59:11 +0100

    Fixed variable to open the actual supporting photo
    Clicking the supporting picture now directs to the supporting picture instead of the main picture

--- wayfarer-review-history-table.user.js
+++ wayfarer-review-history-table.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         Wayfarer Review History Table
-// @version      0.1.3
+// @version      0.1.4
 // @description  Add local review history storage to Wayfarer
 // @namespace    https://github.com/tehstone/wayfarer-addons
 // @homepageURL  https://github.com/tehstone/wayfarer-addons
