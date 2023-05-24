de4e25e8105c7f9f2deabd93912be6de23f92949
tehstone <stone.johnny@gmail.com>
2023-05-24 06:42:17 -0700

    Update wayfarer-review-history-idb.user.js

--- wayfarer-review-history-idb.user.js
+++ wayfarer-review-history-idb.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         Wayfarer Review History IDB
-// @version      0.4.0
+// @version      0.4.1
 // @description  Add local review history storage to Wayfarer
 // @namespace    https://github.com/tehstone/wayfarer-addons
 // @downloadURL  https://github.com/tehstone/wayfarer-addons/raw/main/wayfarer-review-history-idb.user.js
